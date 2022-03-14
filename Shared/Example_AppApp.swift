85b80cbaf022d6ea8c32ccad525857edfa613eb1
Me <fictijps01@gmail.com>
2022-03-14 11:01:48 -0500

    Initial Commit

---
+++ Shared/Example_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_AppApp.swift
+//  Shared
+//
+//  Created by Fictitious on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Example_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
